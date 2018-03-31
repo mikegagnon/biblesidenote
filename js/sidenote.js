fcd84dde8c2c2caac9528b4d1476a7c178e1f511
Michael Gagnon <mikegagnon@gmail.com>
2018-03-31 15:46:10 -0700

    index.html, css, and js

---
+++ js/sidenote.js
@@ -0,0 +1,18 @@
+var Sidenote = {
+
+    constant: {
+        uuidChars: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+        uuidLen: 10,
+    },
+
+    state: {
+    },
+
+    createUuid: function() {
+      var uuid = "";
+      for (var i = 0; i < Sidenote.constant.uuidLen; i++) {
+        uuid += Sidenote.constant.uuidChars.charAt(Math.floor(Math.random() * Sidenote.constant.uuidChars.length));
+      }
+      return uuid;
+    },
+}
